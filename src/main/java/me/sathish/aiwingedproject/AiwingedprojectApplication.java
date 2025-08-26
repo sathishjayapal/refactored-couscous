deafadd4a34676745608c425e1b894f6c47798f8
Bootify <>
2025-08-26 15:09:23 +0000

    initial commit

---
+++ src/main/java/me/sathish/aiwingedproject/AiwingedprojectApplication.java
@@ -0,0 +1,14 @@
+package me.sathish.aiwingedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AiwingedprojectApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(AiwingedprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/sathish/aiwingedproject/AiwingedprojectApplicationTest.java
@@ -0,0 +1,13 @@
+package me.sathish.aiwingedproject;
+
+import me.sathish.aiwingedproject.config.BaseIT;
+import org.junit.jupiter.api.Test;
+
+
+public class AiwingedprojectApplicationTest extends BaseIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
